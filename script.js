56ed89a36a3fec623f2b1802b4d80a671ee98e5f
MeowMeow <147725493+damedane520@users.noreply.github.com>
2023-10-18 16:45:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-// 示例代码，将页面标题更改为欢迎消息
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('h1').textContent = '欢迎来到我的网站！';
+  // 获取按钮元素
+  const fetchButton = document.getElementById('fetchButton');
+
+  // 为按钮添加点击事件监听器
+  fetchButton.addEventListener('click', function() {
+    // 发起 fetch 请求
+    fetch('https://api.example.com/data') // 将URL替换为你的API URL
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('网络请求失败');
+        }
+        return response.json(); // 将响应解析为 JSON
+      })
+      .then(data => {
+        // 处理从后端返回的数据
+        console.log(data); // 在控制台中打印数据，你可以根据需要进行其他操作
+      })
+      .catch(error => {
+        // 处理错误
+        console.error('发生错误:', error);
+      });
+  });
 });
